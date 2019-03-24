bd60eaa9d91973d461359f953cef65cbc332d22f
madeskilz <michaeladekoniye@gmail.com>
2019-03-24 17:24:42 +0100

    abbout

--- application/views/landing/about.php
+++ application/views/landing/about.php
@@ -5,7 +5,7 @@
 					<div class="col-12 col-lg-6">
 						<h3 class="font-weight-light">We believe great things in business are never done by one person. They're done by a team of people.</h3>
 						<p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
-						<a class="button button-lg button-rounded button-dark margin-top-30" href="#">Get In Touch</a>
+						<a class="button button-lg button-rounded button-dark margin-top-30" href="<?= base_url("contact");?>">Get In Touch</a>
 					</div>
 					<div class="col-12 col-lg-6">
 						<div class="row gallery-wrapper spacing-20">
@@ -33,7 +33,7 @@
 		<div class="section bg-grey-lighter">
 			<div class="container">
 				<div class="section-title">
-					<h2 class="font-weight-light">What We Do</h2>
+					<h2 class="font-weight-light">Why Choose Us</h2>
 					<div class="divider-width-50px">
 						<hr class="bg-black-07">
 					</div>
@@ -41,33 +41,33 @@
 				<div class="row col-spacing-40">
 					<div class="col-12 col-md-6 col-lg-4 icon-4xl">
 						<i class="ti-ruler-pencil text-dark"></i>
-						<h5 class="font-weight-normal margin-top-20">Graphic Design</h5>
+						<h5 class="font-weight-normal margin-top-20">Fast Transactions</h5>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+					</div>
+					<div class="col-12 col-md-6 col-lg-4 icon-4xl">
+						<i class="ti-lock text-dark"></i>
+						<h5 class="font-weight-normal margin-top-20">Secured Transactions</h5>
 						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
 					</div>
 					<div class="col-12 col-md-6 col-lg-4 icon-4xl">
 						<i class="ti-camera text-dark"></i>
-						<h5 class="font-weight-normal margin-top-20">Photography</h5>
+						<h5 class="font-weight-normal margin-top-20">Get Alert Real Time</h5>
 						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
 					</div>
 					<div class="col-12 col-md-6 col-lg-4 icon-4xl">
 						<i class="ti-star text-dark"></i>
-						<h5 class="font-weight-normal margin-top-20">Social Marketing</h5>
-						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+						<h5 class="font-weight-normal margin-top-20">Our Services are for You</h5>
+						<p>Our Services are tailored to meet your needs.</p>
 					</div>
 					<div class="col-12 col-md-6 col-lg-4 icon-4xl">
-						<i class="ti-mobile text-dark"></i>
-						<h5 class="font-weight-normal margin-top-20">Mobile App</h5>
-						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+						<i class="ti-world text-dark"></i>
+						<h5 class="font-weight-normal margin-top-20">Fast &amp; Automated Transaction</h5>
+						<p>We offer instant recharge of Airtime, Databundle, CableTV (DStv, GOtv & Startimes), Electricity Bill Payment and so much more at your finger tip.</p>
 					</div>
 					<div class="col-12 col-md-6 col-lg-4 icon-4xl">
-						<i class="ti-settings text-dark"></i>
-						<h5 class="font-weight-normal margin-top-20">Web Development</h5>
-						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
-					</div>
-					<div class="col-12 col-md-6 col-lg-4 icon-4xl">
-						<i class="ti-comment-alt text-dark"></i>
-						<h5 class="font-weight-normal margin-top-20">Consulting</h5>
-						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+						<i class="ti-money text-dark"></i>
+						<h5 class="font-weight-normal margin-top-20">Save More Money</h5>
+						<p>Get amazing discounts and bonuses when you use our platform.</p>
 					</div>
 				</div><!-- end row -->
 			</div><!-- end container -->
