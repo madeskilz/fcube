3f61947b75318d68a56807f06ba6ac6b63bc332d
madeskilz <michaeladekoniye@gmail.com>
2019-03-27 09:10:34 +0100

    user dash

--- application/views/user/data_recharge.php
+++ application/views/user/data_recharge.php
@@ -1,5 +1,5 @@
 <?php $this->load->view('user/inc/sidebar');?>
-	<div style="min-height:92vh;">
+	<div style="min-height:calc(100vh - 358px);">
     
     </div>		
 <?php $this->load->view('user/inc/footer');?>
